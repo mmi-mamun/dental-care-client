44494e68c247cf5658fb25dd983eac924d0388d5
mmi-mamun <muzahidulislam735@gmail.com>
2022-11-08 22:23:13 +0600

    Login and signUp page done

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
-import img from '../../images/others/login.png'
+import imgSignUp from '../../images/others/signup.jpg';
 
-const Login = () => {
-    const { logIn } = useContext(AuthContext);
-    const handleLogin = event => {
+const Signup = () => {
+    const { createUser } = useContext(AuthContext);
+    const handleSignUp = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
 
-        logIn(email, password)
+        createUser(email, password)
             .then(result => {
                 const user = result.user;
                 console.log(user)
@@ -22,31 +22,38 @@ const Login = () => {
         <div className="hero w-full bg-base-200">
             <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left mx-auto">
-                    <img className='rounded-lg' src={img} alt="" />
+                    <img className='rounded-lg' src={imgSignUp} alt="" />
                 </div>
                 <div className="mx-auto card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-5">
-                    <h1 className="text-5xl font-bold mx-auto">Login now!</h1>
-                    <form onSubmit={handleLogin} className="card-body">
+                    <h1 className="text-5xl font-bold mx-auto">SIGN UP</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Your Name</span>
+                            </label>
+                            <input name='name' type="text" placeholder="Enter your name" className="input input-bordered" required />
+                        </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input name='email' type="text" placeholder="email" className="input input-bordered" required />
+                            <input name='email' type="text" placeholder="Enter your email" className="input input-bordered" required />
                         </div>
+
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input name='password' type="text" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <input className='btn btn-primary' type="submit" value="Login" />
+                            <input name='password' type="text" placeholder="Enter a new password" className="input input-bordered" required />
                         </div>
 
-                        <p className='text-center my-3'>New to dental service?.. <Link to='/signup' className='text-orange-600'>Sign up</Link></p>
+                        <div className="form-control mt-6">
+                            <input className='btn btn-primary' type="submit" value="Sign up" />
+                        </div>
+
+                        <p className='text-center my-3'>Already have an account?.. <Link to='/login' className='text-orange-600'>Please Login</Link></p>
                     </form>
                 </div>
             </div>
@@ -54,4 +61,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
--- src/router/Router/Router.js
+++ src/router/Router/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../pages/Home/Home";
 import Login from "../../pages/Login/Login";
+import Signup from "../../pages/Signup/Signup";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
             }
         ]
     }
