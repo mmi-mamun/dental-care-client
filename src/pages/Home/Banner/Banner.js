9418935ca325acbd3900b7749375e7f7c683db2d
mmi-mamun <muzahidulislam735@gmail.com>
2022-11-08 07:27:37 +0600

    Sliders are added

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Banner.css'
+import banner1 from '../../../images/banner/banner-1.jpg'
+import banner2 from '../../../images/banner/banner-2.jpg'
+import banner3 from '../../../images/banner/banner-3.jpg'
+import banner4 from '../../../images/banner/banner-4.jpg'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full my-3">
+                <div id="item1" className="carousel-item relative w-full">
+                    <div className="img-gradient-right w-full  rounded-xl">
+                        <img src={banner1} className="w-full rounded-xl" alt="" />
+                    </div>
+                    <div className='absolute text-end transform -translate-y-1/2 left-5 right-5 top-1/4'>
+                        <h1 className='text-5xl font-semibold my-5'> Expert surgical care <br />  you know and trust</h1>
+                        <h2 className='text-2xl font-semibold'>​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​Dental or oral health is concerned with your teeth, gums and mouth. <br /> The goal is to prevent complications</h2>
+                    </div>
+                </div>
+
+                <div id="item2" className="carousel-item relative w-full">
+                    <div className="img-gradient-bottom w-full  rounded-xl">
+                        <img src={banner2} className="w-full rounded-xl" alt="" />
+                    </div>
+                    <div className='absolute text-center transform -translate-y-1/2 left-1/4 top-3/4'>
+                        <h1 className='text-5xl font-semibold my-5'> Expert surgical care <br />  you know and trust</h1>
+                        <h2 className='text-2xl font-semibold'>​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​Dental or oral health is concerned with your teeth, gums and mouth. <br /> The goal is to prevent complications</h2>
+                    </div>
+
+                </div>
+                <div id="item3" className="carousel-item relative w-full">
+                    <div className="img-gradient-left w-full  rounded-xl">
+                        <img src={banner3} className="w-full rounded-xl" alt='' />
+
+                    </div>
+                    <div className='absolute text-start transform -translate-y-1/2 left-5 top-2/4'>
+                        <h1 className='text-5xl font-semibold my-5'> Expert surgical care <br />  you know and trust</h1>
+                        <h2 className='text-2xl font-semibold'>​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​Dental or oral health is concerned with your teeth, gums and mouth. <br /> The goal is to prevent complications</h2>
+                    </div>
+                </div>
+                <div id="item4" className="carousel-item relative w-full">
+                    <div className="img-gradient-right w-full">
+                        <img src={banner4} className="w-full rounded-xl" alt='' />
+                    </div>
+                    <div className='absolute text-end transform -translate-y-1/2 right-12 top-1/4'>
+                        <h1 className='text-5xl font-semibold my-5'> Expert surgical care <br />  you know and trust</h1>
+                        <h2 className='text-2xl font-semibold'>​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​Dental or oral health is concerned with your teeth, gums and mouth. <br /> The goal is to prevent complications</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+                <a href="#item4" className="btn btn-xs">4</a>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
