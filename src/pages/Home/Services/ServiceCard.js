f4383dbf53ef936bab157b57f3b6eb4db436b6b8
mmi-mamun <muzahidulislam735@gmail.com>
2022-11-10 18:24:38 +0600

    React photo viewer finish

--- src/pages/Home/Services/ServiceCard.js
+++ src/pages/Home/Services/ServiceCard.js
@@ -7,19 +7,23 @@ import { PhotoProvider, PhotoView } from 'react-photo-view';
 const ServiceCard = ({ service }) => {
     const { _id, serviceName, img, price, description } = service;
     return (
-        <div className="card w-96 glass mx-auto">
-            <figure><img src={img} alt="car!" /></figure>
-            <div className="card-body">
-                <h2 className="card-title font-bold text-center text-3xl">{serviceName}</h2>
-                <p className='text-2xl font-semibold text-center text-orange-600'>Fee: {price} Taka</p>
-                <p className='text-justify'>{description.slice(0, 100) + '...'}</p>
-                <div className="card-actions justify-center">
-                    <Link to={`/services/${_id}`}>
-                        <button className="btn btn-primary text-xl bg-orange-500 text-black ">View Details</button>
-                    </Link>
+        <PhotoProvider>
+            <div className="card w-96 glass mx-auto">
+                <PhotoView src={img}>
+                    <img src={img} alt="car!" />
+                </PhotoView>
+                <div className="card-body">
+                    <h2 className="card-title font-bold text-center text-3xl">{serviceName}</h2>
+                    <p className='text-2xl font-semibold text-center text-orange-600'>Fee: {price} Taka</p>
+                    <p className='text-justify'>{description.slice(0, 100) + '...'}</p>
+                    <div className="card-actions justify-center">
+                        <Link to={`/services/${_id}`}>
+                            <button className="btn btn-primary text-xl bg-orange-500 text-black ">View Details</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </PhotoProvider>
     );
 };
 
